6c12daad230d0c5e23fefb2b7ce8b56dca12646e
Luca Ciampi <91676870+LucaCiampi@users.noreply.github.com>
2024-10-20 16:02:20 +0200

    🎨 added type on usePersons

--- src/hooks/usePersons.tsx
+++ src/hooks/usePersons.tsx
@@ -1,26 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { API_BASE_URL } from '../constants/config';
+import { Person } from '../types/Person';
 
 const usePersons = (amount: number = 12) => {
-  const [persons, setPersons] = useState<any[]>([]);
+  const [persons, setPersons] = useState<Person[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchPersons = async () => {
-      try {
-        const response = await axios.get(API_BASE_URL + '?results=' + amount);
-        setPersons(response.data.results);
-      } catch (err: any) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchPersons();
+  const fetchPersons = useCallback(async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}?results=${amount}`);
+      setPersons(response.data.results);
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
   }, [amount]);
+
+  useEffect(() => {
+    fetchPersons();
+  }, [fetchPersons]);
 
   return { persons, loading, error };
 };
